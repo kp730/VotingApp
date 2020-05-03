b5496185fe19fca7f3e950be55c88fa31e25c6d7
kp730 <krish170123028@iitg.ac.in>
2020-05-03 18:16:07 +0530

    Added basic tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,24 @@ pragma solidity 0.5.16;
 
 contract Election
 {
-    string public candidate;
+    // Model a candidate
+    struct Candidate
+    {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    // Storing and fetching candidates
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
     constructor () public
     {
-        candidate = "KP";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+    function addCandidate(string memory _name) private
+    {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
